0efcb9f6b02278d8c3404a77dbe4131025d6d18a
thetnaingtn <thetnaingtun.ucsy@gmail.com>
2026-05-26 14:16:56 +0700

    feat: implement EmbedderClient interface

--- embeddings.go
+++ embeddings.go
@@ -1,13 +1,28 @@
 package kuzco
 
 import (
+	"context"
 	"errors"
+	"fmt"
 	"sort"
 
 	"github.com/ardanlabs/kronk/sdk/kronk/model"
 )
 
 var errEmptyInput = errors.New("kuzco: embeddings: texts must not be empty")
+
+func (l *LLM) CreateEmbedding(ctx context.Context, texts []string) ([][]float32, error) {
+	if len(texts) == 0 {
+		return nil, errEmptyInput
+	}
+	ctx, cancel := l.ensureDeadline(ctx)
+	defer cancel()
+	resp, err := l.k.Embeddings(ctx, model.D{"input": texts})
+	if err != nil {
+		return nil, fmt.Errorf("kuzco: embeddings: %w", err)
+	}
+	return embedResponseToVectors(resp), nil
+}
 
 func embedResponseToVectors(resp model.EmbedReponse) [][]float32 {
 	data := make([]model.EmbedData, len(resp.Data))
